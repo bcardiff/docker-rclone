2919d4d39f78728b4b4b46ece772de983a315dc3
Brian J. Cardiff <bcardiff@manas.tech>
2017-09-18 10:56:44 -0300

    Avoid running sync if source directory is empty

--- sync.sh
+++ sync.sh
@@ -2,5 +2,11 @@
 
 set -e
 
-echo "INFO: Starting rclone sync $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS"
-rclone sync $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS
+if test "$(rclone ls $SYNC_SRC $RCLONE_OPTS)"; then
+  # the source directory is not empty
+  # it can be synced without clear data loss
+  echo "INFO: Starting rclone sync $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS"
+  rclone sync $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS
+else
+  echo "WARNING: Source directory is empty. Skipping sync command."
+fi
