5a421d9beef4d97d8fa3f7ff9e620c681822242a
Brian J. Cardiff <bcardiff@manas.tech>
2017-09-18 11:01:32 -0300

    Prevent multiple sync commands at the same time

--- sync.sh
+++ sync.sh
@@ -4,6 +4,10 @@ set -e
 
 echo "INFO: Starting sync.sh $(date)"
 
+if [ `lsof | grep $0 | wc -l | tr -d ' '` -gt 1 ]
+then
+  echo "WARNING: A previous sync is still running. Skipping new sync command."
+else
 if test "$(rclone ls $SYNC_SRC $RCLONE_OPTS)"; then
   # the source directory is not empty
   # it can be synced without clear data loss
@@ -11,4 +15,6 @@ if test "$(rclone ls $SYNC_SRC $RCLONE_OPTS)"; then
   rclone sync $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS
 else
   echo "WARNING: Source directory is empty. Skipping sync command."
+fi
+
 fi
