d3fb318f9c3ef658e8e8208a5ea5f4e53cb83f9f
pfidr34 <dpfister@gmail.com>
2019-05-01 15:59:42 -0500

    Change variable name

--- sync.sh
+++ sync.sh
@@ -5,10 +5,10 @@ set -e
 echo "INFO: Starting sync.sh pid $$ $(date)"
 
 # Delete logs by user request
-if [ ! -z "${ROTATE_LOGS##*[!0-9]*}" ]
+if [ ! -z "${ROTATE_LOG##*[!0-9]*}" ]
 then
-  echo "INFO: Removing logs older than $ROTATE_LOGS day(s)..."
-  find /logs/*.txt -mtime +$ROTATE_LOGS -type f -delete
+  echo "INFO: Removing logs older than $ROTATE_LOG day(s)..."
+  find /logs/*.txt -mtime +$ROTATE_LOG -type f -delete
 fi
 
 if [ `lsof | grep $0 | wc -l | tr -d ' '` -gt 1 ]
