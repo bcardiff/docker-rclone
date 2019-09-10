dc45a6281e1b5f019b4d2fe8e5d1fa66353bec06
pfidr34 <pfidr34@users.noreply.github.com>
2019-09-10 08:53:30 -0500

    Change log directory check

--- sync.sh
+++ sync.sh
@@ -8,7 +8,7 @@ echo "INFO: Starting sync.sh pid $$ $(date)"
 if [ ! -z "${ROTATE_LOG##*[!0-9]*}" ]
 then
   echo "INFO: Removing logs older than $ROTATE_LOG day(s)..."
-  test -d "/logs/*.txt" && find /logs/*.txt -mtime +$ROTATE_LOG -type f -delete
+  touch /logs/tmp.txt && find /logs/*.txt -mtime +$ROTATE_LOG -type f -delete
 fi
 
 if [ `lsof | grep $0 | wc -l | tr -d ' '` -gt 1 ]
