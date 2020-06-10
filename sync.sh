2ee0220236026b5fed502b8fa807a8b3d6cb0f63
Dante Barba <dantebarba@gmail.com>
2020-06-10 18:53:48 -0300

    remove sync warning

--- sync.sh
+++ sync.sh
@@ -9,25 +9,21 @@ else
 
 echo $$ > /tmp/sync.pid
 
-if test "$(rclone ls --max-depth 1 $SYNC_SRC $RCLONE_OPTS)"; then
-  # the source directory is not empty
-  # it can be synced without clear data loss
-  echo "INFO: Starting rclone $COMMAND $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS"
-  rclone $COMMAND $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS
-  export RETURN_CODE=$?
-  if [ -z "$CHECK_URL" ]
-  then
-    echo "INFO: Define CHECK_URL with https://healthchecks.io to monitor sync job"
-  else
-    if [ "$RETURN_CODE" == 0 ]
-    then
-      wget $CHECK_URL -O /dev/null
-    else
-      wget $FAIL_URL -O /dev/null
-    fi
-  fi
+# the source directory is not empty
+# it can be synced without clear data loss
+echo "INFO: Starting rclone $COMMAND $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS"
+rclone $COMMAND $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS
+export RETURN_CODE=$?
+if [ -z "$CHECK_URL" ]
+then
+  echo "INFO: Define CHECK_URL with https://healthchecks.io to monitor sync job"
 else
-  echo "WARNING: Source directory is empty. Skipping sync command."
+  if [ "$RETURN_CODE" == 0 ]
+  then
+    wget $CHECK_URL -O /dev/null
+  else
+    wget $FAIL_URL -O /dev/null
+  fi
 fi
 
 rm -f /tmp/sync.pid
